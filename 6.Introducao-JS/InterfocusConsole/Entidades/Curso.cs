d146a032ee93b66f46974de00ac973f4f350c697
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-05-19 03:02:48 -0300

    ambiente preparado

---
+++ 6.Introducao-JS/InterfocusConsole/Entidades/Curso.cs
@@ -0,0 +1,26 @@
+﻿namespace InterfocusConsole.Entidades
+{
+    public class Curso
+    {
+        public virtual int Id { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual string Descricao { get; set; }
+        // 0 - iniciante, 1 - intermediario, 2 - avançado, 3 - expert
+        public virtual NivelCurso Nivel { get; set; }
+        public virtual int Duracao { get; set; }
+
+        void UsandoEnum()
+        {
+            if (Nivel == NivelCurso.Iniciante) { } // faz alguma coisa
+            else if (Nivel == NivelCurso.Intermediario) { } // faz outra coisa 
+        }
+    }
+
+    public enum NivelCurso
+    {
+        Iniciante = 0,
+        Intermediario = 1,
+        Avancado = 2,
+        Expert = 3
+    }
+}
---
+++ 6.Introducao-JS/InterfocusConsole/Services/CursoService.cs
@@ -0,0 +1,109 @@
+﻿using InterfocusConsole.Entidades;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfocusConsole.Services
+{
+    public class CursoService
+    {
+        private readonly ISessionFactory session;
+
+        public CursoService(ISessionFactory session)
+        {
+            this.session = session;
+        }
+
+        public static bool Validacao(Curso aluno,
+            out List<ValidationResult> erros)
+        {
+            erros = new List<ValidationResult>();
+            var valido = Validator.TryValidateObject(aluno,
+                new ValidationContext(aluno),
+                erros,
+                true
+            );
+
+            return valido;
+        }
+
+        public virtual List<Curso> Listar()
+        {
+            using var sessao = session.OpenSession();
+            var cursos = sessao.Query<Curso>()
+                               .OrderBy(curso => curso.Id)
+                               .ToList();
+            return cursos;
+        }
+
+        public virtual List<Curso> Listar(string busca)
+        {
+            using var sessao = session.OpenSession();
+            var cursos = sessao.Query<Curso>()
+                               .Where(c => c.Nome.Contains(busca) || c.Descricao.Contains(busca))
+                               .OrderBy(curso => curso.Id)
+                               .ToList();
+            return cursos;
+        }
+
+        public virtual bool Criar(Curso curso, out List<ValidationResult> erros)
+        {
+            if (Validacao(curso, out erros))
+            {
+                using var sessao = session.OpenSession();
+                using var transaction = sessao.BeginTransaction();
+
+                sessao.Save(curso);
+                transaction.Commit();
+                return true;
+            }
+            return false;
+        }
+
+        public virtual bool Editar(Curso curso, out List<ValidationResult> erros)
+        {
+            if (Validacao(curso, out erros))
+            {
+                using var sessao = session.OpenSession();
+                using var transaction = sessao.BeginTransaction();
+
+                sessao.Merge(curso);
+                transaction.Commit();
+                return true;
+            }
+            return false;
+        }
+
+        public virtual bool Excluir(int id, out List<ValidationResult> erros)
+        {
+            erros = new List<ValidationResult>();
+            using var sessao = session.OpenSession();
+            using var transaction = sessao.BeginTransaction();
+            var curso = sessao.Query<Curso>()
+                .Where(c => c.Id == id)
+                .FirstOrDefault();
+            if (curso == null)
+            {
+                erros.Add(new ValidationResult($"Registro {id} não encontrado :(", new[] { "id" }));
+                return false;
+            }
+
+            sessao.Delete(curso);
+            try
+            {
+                transaction.Commit();
+            } catch (Exception ex)
+            {
+                erros.Add(new ValidationResult($"N vai da, por causa das constraints fk :("));
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
