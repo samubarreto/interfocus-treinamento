43cc7667beb9f963563de4e1f3440b4addaf5cc2
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-05-26 16:07:47 -0300

    js modificado para pegar registro selecionado, alterar indice dele e limpar campos de input... uma loucura

--- 6.Introducao-JS/FrontEnd/scripts/curso.js
+++ 6.Introducao-JS/FrontEnd/scripts/curso.js
@@ -1,45 +1,44 @@
 function addCurso(evento) {
+	evento.preventDefault();
+
 	var dados = new FormData(evento.target);
+	var tabela = document.querySelector("tbody");
+	var index = Number(dados.get("index"));
 
-	var linha = `
-	<tr>
-		<td>${dados.get("nome")}</td>
-		<td>${dados.get("duracao")}</td>
-		<td>${dados.get("nivel")}</td>
-	</tr>
-	`;
-
-	var tabela = document.getElementsByTagName("table")[0];
-	tabela.innerHTML += linha;
-
-	var indice = Number(dados.get("index"));
-	if (indice > -1) {
-		var nodeTr = document.querySelector("tbody").children.item(indice);
-
+	if (index > -1) {
+		var nodeTr = tabela.children[index];
 		var [nome, duracao, nivel] = nodeTr.children;
 		nome.innerHTML = dados.get("nome");
 		duracao.innerHTML = dados.get("duracao");
 		nivel.innerHTML = dados.get("nivel");
-		evento.target.reset();
+	} else {
+		var linha = document.createElement("tr");
+		linha.innerHTML = `
+					<td>${dados.get("nome")}</td>
+					<td>${dados.get("duracao")}</td>
+					<td>${dados.get("nivel")}</td>
+			`;
+		tabela.appendChild(linha);
 	}
 
-	evento.preventDefault();
-
+	evento.target.reset();
+	evento.target.index.value = -1;
 }
 
 function selecionar(evento) {
-	// var { target, source } = evento;
+	var target = evento.target; // nao entendi
 
-	var { target } = evento;
-	var nodeTR = target.closest("tr");
+	var selected = target.closest("tbody").querySelector("tr.selecionado");
+	selected?.classList.remove("selecionado"); // remove a classe selecionado de outro tr caso tenha
 
-	var index = [...target.closest("tbody").querySelectorAll("tr")].indexOf(nodeTR); // <- zica
-	console.log(index);
+	var nodeTR = target.closest("tr"); // pega o tr mais próximo da td clicada
+	nodeTR.classList.add("selecionado"); // adiciona classe selecionado a tr clicada, bizarro
+
+	var index = [...target.closest("tbody").querySelectorAll("tr")].indexOf(nodeTR); // pega o index do tr selecionado sei lá como, bizarro
 
 	var [nome, duracao, nivel] = nodeTR.querySelectorAll("td");
 
 	var form = document.querySelector("form");
-
 	var campoNome = form.querySelector("input[name=nome]");
 	var campoDuracao = form.querySelector("input[name=duracao]");
 	var campoNivel = form.querySelector("input[name=nivel]");
@@ -48,6 +47,7 @@ function selecionar(evento) {
 	campoNome.value = nome.innerText;
 	campoDuracao.value = duracao.innerText;
 	campoNivel.value = nivel.innerText;
-}
+	campoIndice.value = index;
 
-// FAZER: pegar registro selecionado, alterar indice dele e limpar campos de input
+	console.log(index, nodeTR, selected); // insano
+}
