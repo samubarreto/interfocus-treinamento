80318260ead9ba0bae3b874c9739b38a1f09afe8
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-05-25 22:51:14 -0300

    intro ao js, muito forms e forms junto a um grid, falta fazer funcionar como esperado...

--- 6.Introducao-JS/InterfocusConsole/Entidades/Curso.cs
+++ 6.Introducao-JS/InterfocusConsole/Entidades/Curso.cs
@@ -2,16 +2,16 @@
 {
     public class Curso
     {
-        public virtual int Id { get; set; }
-        public virtual string Nome { get; set; }
-        public virtual string Descricao { get; set; }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
         // 0 - iniciante, 1 - intermediario, 2 - avançado, 3 - expert
-        public virtual NivelCurso Nivel { get; set; }
-        public virtual int Duracao { get; set; }
+        public NivelCurso Nivel { get; set; }
+        public int Duracao { get; set; }
 
-        void UsandoEnum()
+        void Metodo()
         {
-            if (Nivel == NivelCurso.Iniciante) { } // faz alguma coisa
+            if (Nivel == NivelCurso.Iniciante) { }// faz alguma coisa
             else if (Nivel == NivelCurso.Intermediario) { } // faz outra coisa 
         }
     }
