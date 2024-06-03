d47d816fb213554d25e478b51b27f10c86c05cc3
Samuel Pereira Barreto <samu.barreto2004@gmail.com>
2024-06-02 21:19:56 -0300

    pequenas mudanças

--- 8.Introducao-React/FrontEnd/server.js
+++ 8.Introducao-React/FrontEnd/server.js
@@ -1,24 +1,15 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var express = require("express");
+var app = express();
+app.use(express.static('.'));
 
-// Middleware para servir arquivos estáticos
-app.use(express.static(path.join(__dirname, '..')));
-
-// Rota para arquivos estáticos
 app.get(/.+\.(js|ico|html|png|css|map)$/, function (req, res) {
-    const filePath = path.join(__dirname, req.originalUrl);
-    res.sendFile(filePath);
+    res.sendFile(req.originalUrl.replace(/^./, ""));
 });
-
-// Rota para todas as outras requisições
 app.get(/.+$/, function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
+app.listen(3000);
 
 // npm i express
 // node server
